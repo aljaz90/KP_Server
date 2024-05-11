4d6d56f3ecb3e8f209cb4d457d88f53e09a8c963
aljaz90 <aljaz1.kern@gmail.com>
2024-05-11 19:46:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { createHandler } = require('graphql-http/lib/use/express');
+const schema = require('./graphql/Schema');
+const resolvers = require('./graphql/Resolvers');
+
+mongoose.connect('mongodb://localhost:27017/kp-rest-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+const server = express();
+server.all('/graphql', createHandler({ schema: schema, rootValue: resolvers }));
+
+server.listen(4000, () => {
+    console.log('Listening to port 4000');
+});
