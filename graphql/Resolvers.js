60996f907720baf6a58840be1b1fd5b8a7e31132
aljaz90 <aljaz1.kern@gmail.com>
2024-05-12 10:41:38 +0200

    Update schemas and resolvers

--- graphql/Resolvers.js
+++ graphql/Resolvers.js
@@ -1,51 +1,98 @@
 const User = require("../models/User");
 
 const resolvers = {
-  getUser: async ({ id }) => {
-    try {
-      const user = await User.findById(id);
-      return user;
-    } catch (err) {
-      throw new Error("Error retrieving user");
-    }
-  },
-  getUsers: async () => {
-    try {
-      const users = await User.find();
-      return users;
-    } catch (err) {
-      throw new Error("Error retrieving users");
-    }
-  },
-  createUser: async ({ name, email, password }) => {
-    try {
-      const user = new User({ name, email, password });
-      await user.save();
-      return user;
-    } catch (err) {
-      throw new Error("Error creating user");
-    }
-  },
-  updateUser: async ({ id, name, email, password }) => {
-    try {
-      const user = await User.findByIdAndUpdate(
-        id,
-        { name, email, password },
-        { new: true }
-      );
-      return user;
-    } catch (err) {
-      throw new Error("Error updating user");
-    }
-  },
-  deleteUser: async ({ id }) => {
-    try {
-      const user = await User.findByIdAndRemove(id);
-      return user;
-    } catch (err) {
-      throw new Error("Error deleting user");
-    }
-  },
+    // User
+    getUser: async ({ id }) => {
+        try {
+            const user = await User.findById(id);
+            return user;
+        } catch (err) {
+            throw new Error("Error retrieving user");
+        }
+    },
+    getUsers: async () => {
+        try {
+            const users = await User.find();
+            return users;
+        } catch (err) {
+            throw new Error("Error retrieving users");
+        }
+    },
+    createUser: async ({ name, email, password }) => {
+        try {
+            const user = new User({ name, email, password });
+            await user.save();
+            return user;
+        } catch (err) {
+            throw new Error("Error creating user");
+        }
+    },
+    updateUser: async ({ id, name, email, password }) => {
+        try {
+            const user = await User.findByIdAndUpdate(
+                id,
+                { name, email, password },
+                { new: true }
+            );
+            return user;
+        } catch (err) {
+            throw new Error("Error updating user");
+        }
+    },
+    deleteUser: async ({ id }) => {
+        try {
+            const user = await User.findByIdAndRemove(id);
+            return user;
+        } catch (err) {
+            throw new Error("Error deleting user");
+        }
+    },
+    // Invoice
+    getInvoice: async ({ id }) => {
+        try {
+            const invoice = await Invoice.findById(id);
+            return invoice;
+        } catch (err) {
+            throw new Error("Error retrieving invoice");
+        }
+    },
+    getInvoices: async () => {
+        try {
+            const invoices = await Invoice.find();
+            return invoices;
+        } catch (err) {
+            throw new Error("Error retrieving invoices");
+        }
+    },
+    createInvoice: async ({ amount, paid_at, user }) => {
+        try {
+            const invoice = new Invoice({ amount, paid_at, user });
+            await invoice.save();
+            return invoice;
+        } catch (err) {
+            throw new Error("Error creating invoice");
+        }
+    },
+    updateInvoice: async ({ id, amount, paid_at, user }) => {
+        try {
+            const invoice = await Invoice.findByIdAndUpdate(
+                id,
+                { amount, paid_at, user },
+                { new: true }
+            );
+            return invoice;
+        } catch (err) {
+            throw new Error("Error updating invoice");
+        }
+    },
+    deleteInvoice: async ({ id }) => {
+        try {
+            const invoice = await Invoice.findByIdAndRemove(id);
+            return invoice;
+        } catch (err) {
+            throw new Error("Error deleting invoice");
+        }
+    },
 };
 
 module.exports = resolvers;
--- models/Invoice.js
+++ models/Invoice.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const invoiceSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   }
+    amount: {
+        type: Number,
+        required: true
+    },
+    paid_at: {
+        type: Date,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.ObjectId, 
+        ref: 'User',
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Invoice', invoiceSchema);
